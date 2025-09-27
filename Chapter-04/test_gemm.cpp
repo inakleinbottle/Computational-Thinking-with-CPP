718b00da4cd99649c45b235f09517f5500ac49e6
Sam Morley <admin@inakleinbottle.com>
2025-09-27 12:58:16 +0100

    added a couple more tests for gemm

--- Chapter-04/test_gemm.cpp
+++ Chapter-04/test_gemm.cpp
@@ -2,10 +2,12 @@
 // Created by sammorley on 31/12/24.
 //
 
+#include "gemm.h"
+
+#include <random>
 
 #include <gtest/gtest.h>
 
-#include "gemm.h"
 #include "matrix.h"
 
 using namespace ct;
@@ -33,6 +35,58 @@ protected:
 };
 
 
+TEST(BasicGemmTest, TestMulIdentity) {
+    const auto n_rows = 10;
+    const auto n_cols = 10;
+    Matrix<double> A(n_rows, n_cols);
+    std::mt19937 rng(std::random_device{}());
+    std::uniform_real_distribution<double> dist(-1., 1.);
+    for (auto& entry : A.to_span()) {
+        entry = dist(rng);
+    }
+
+    Matrix<double> I(n_rows, n_cols);
+    auto* I_data = I.data();
+    for (size_t i=0; i < n_rows; i++) {
+        I_data[i*(n_rows + 1)] = 1.0;
+    }
+
+    Matrix<double> left_result(n_rows, n_cols);
+    dgemm_basic(A, I, left_result, 1.0, 0.0);
+
+    Matrix<double> right_result(n_rows, n_cols);
+    dgemm_basic(I, A, right_result, 1.0, 0.0);
+
+    EXPECT_LT(matrix_max_abs_difference(A, left_result), 1e-15);
+
+    EXPECT_LT(matrix_max_abs_difference(A, right_result), 1e-15);
+}
+
+
+TEST(BasicGemmTest, TestAgainstInverse) {
+    const auto n_rows = 3;
+    const auto n_cols = 3;
+    // working out a 3x3 inverse by hand is not too much of a chore,
+    // but let's not risk a random matrix that might be singular.
+    Matrix<double> A(n_rows, n_cols);
+    A.at(0, 0) = 1.0;
+    A.at(0, 1) = -1.0;
+    A.at(0, 2) = 0.0;
+    A.at(1, 0) = 0.5;
+    A.at(1, 1) = 0.5;
+    A.at(1, 2) = 3;
+    A.at(2, 0) = -0.5;
+    A.at(2, 1) = 0.5;
+    A.at(2, 2) = 2.0;
+
+    // expand along the top row to compute the determinant. The result should be 2
+    auto detA = A.at(0,0)*(A.at(1, 1) * A.at(2,2) - A.at(1,2)*A.at(2, 1))
+        - A.at(0, 1) * (A.at(1, 0) * A.at(2, 2) - A.at(1, 2)*A.at(2, 0));
+
+    EXPECT_EQ(detA, 2.0);
+
+
+}
 
 
 TEST_F(GemmTest, TestBlockedAgainstBasic) {
